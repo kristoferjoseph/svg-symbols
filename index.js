bc786aa7b1a5cf562513158d5cfe304768214b01
kj <kristoferjoseph@gmail.com>
2017-12-07 11:11:17 -0800

    Patch fixes issue where camelCase is required for writing viewBox but will not work when reading viewbox from an svg tag.

--- index.js
+++ index.js
@@ -5,41 +5,40 @@ var dom = require('cheerio')
 var args = process.argv.slice(2)
 var $ = dom.load('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0" style="display:none;"></svg>')
 var dir
-var file
 var fileName
 var svgNode
 var symbolNode
+
+function parse (file) {
+  if (path.extname(file) === '.svg') {
+    fileName = file.slice(0, -4)
+    file = fs.readFileSync(path.join(dir, file), 'utf8')
+    svgNode = $(file)
+    symbolNode = $('<symbol></symbol>')
+    symbolNode.attr('viewBox', svgNode.attr('viewbox'))
+    symbolNode.attr('id', fileName)
+    symbolNode.append(svgNode.contents())
+    symbolNode
+      .children()
+      .each(function (i, kid) {
+        $(kid)
+          .removeAttr('fill')
+          .removeAttr('stroke')
+      })
+    $('svg').append(symbolNode)
+  }
+}
+
 if (args && args.length) {
   dir = args[0]
-  fs.readdir(dir, function(err, files) {
+  fs.readdir(dir, function (err, files) {
     if (err) {
       process.stderr(err)
       return
     }
-    files.forEach(function(file) {
-      if (path.extname(file) === '.svg') {
-        fileName = file.slice(0, -4)
-        file = fs.readFileSync(path.join(dir, file), 'utf8')
-        svgNode = $(file)
-        symbolNode = $('<symbol></symbol>')
-        symbolNode.attr('viewbox', svgNode.attr('viewbox'))
-        symbolNode.attr('id', fileName)
-        symbolNode.append(svgNode.contents())
-        symbolNode
-          .children()
-          .each(
-            function(i, kid) {
-              $(kid)
-                .removeAttr('fill')
-                .removeAttr('stroke')
-            }
-          )
-        $('svg').append(symbolNode)
-      }
-    })
+    files.forEach(parse)
     process.stdout.write($.html())
   })
-}
-else {
+} else {
   process.stderr('Directory not found.')
 }
--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var nixt = require('nixt')
 var fixture = fs.readFileSync('./out.svg', 'utf8')
 
 // To run tests you need to run `npm i -g` from the root of this module
-test('should have correct output', function(t) {
+test('should have correct output', function (t) {
   nixt()
-    .expect(function(result) {
-      t.equals(result.stdout, fixture)
+    .expect(function (result) {
+      t.equals(result.stdout, fixture.trim())
     })
     .run('svg-symbols ./fixtures')
     .end(t.end)
